e704dd2d11e51e4232fd19e05b60e6513377408c
razielrodrigues <raziel@upoutsourcing.com>
2022-05-04 11:14:54 -0300

    primeiro componente

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,17 @@
-import { useState } from 'react'
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button className="bg-violet-500 p-2">{props.text ?? 'Default'}</button>
+}
 
 function App() {
-  return <h1>Hello World</h1>;
+  return <div>
+    <Button text="Enviar" />,
+    <Button text="Delete" />,
+    <Button />
+  </div>
 }
 
 export default App
