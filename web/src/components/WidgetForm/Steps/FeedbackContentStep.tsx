c24ef97d29d0112ecf986ad2a9473d4e7fa18a5e
razielrodrigues <raziel@upoutsourcing.com>
2022-05-06 01:40:08 -0300

    wip

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -2,7 +2,7 @@ import { CloseButton } from "../../CloseButton";
 import { FeedbackType, feedbackTypes } from '..';
 import { ArrowLeft } from "phosphor-react";
 import { ScreenShotButton } from './../ScreenShotButton';
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType,
@@ -12,8 +12,17 @@ interface FeedbackContentStepProps {
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
 
     const [screenshot, setScreenshot] = useState<string | null>(null)
+    const [comment, setComment] = useState<string>('')
 
     const feedbackTypesInfo = feedbackTypes[feedbackType];
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault();
+        console.log({
+            screenshot,
+            comment
+        })
+    }
 
     return (
         <>
@@ -39,12 +48,13 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }
                 <CloseButton />
             </header>
 
-            <form className=" my-4 w-full">
+            <form onSubmit={handleSubmitFeedback} className=" my-4 w-full">
                 <textarea
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus-ring-1 resize-none focus:outline-none scrollbar  scrollbar-thumb-zinc-700
                 scrollbar-track-transparent scrollbar-thin
                 "
-                    placeholder="Conte com detalhe o que esta acontecendo..."
+                    placeholder="Conte com detalhes o que está acontecendo..."
+                    onChange={event => setComment(event.target.value)}
                 />
 
                 <footer className="flex gap-2 mt-2">
