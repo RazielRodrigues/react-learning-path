8d4970d5e37acd02f26aa22ccf9bf1c3b392fce9
razielrodrigues <raziel@upoutsourcing.com>
2022-05-08 13:40:02 -0300

    configurando envio de email

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,47 @@
 import express from 'express';
+import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Middleware
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "4534d3e6df3a49",
+      pass: "a85fc9555674a2"
+    }
+  });
+
+app.post('/feedbacks', async (req, res) => {
+
+    const {type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot
+        }
+    })
+
+    transport.sendMail({
+        from: 'Equipe do RZ <razielx3@live.com>',
+        to: 'razielx3@live.com',
+        subject: 'Feedback do RZ',
+        html: [
+            `<div style="text-align: center;">`,
+                `<h1>tipo: ${type}</h1>`,
+                `<h1>comentario: ${comment}</h1>`,
+                `<h1>foto: ${screenshot}</h1>`,
+            `</div>`,
+        ].join('\n')
+    });
+
+    return res.status(201).json({data: feedback});
 });
 
 app.listen(3333, () => {
