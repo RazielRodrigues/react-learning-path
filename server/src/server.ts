158b372e3e51a1e576f5a9602a278d2846abfee7
razielrodrigues <raziel@upoutsourcing.com>
2022-05-08 15:17:51 -0300

    wip

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { routes } from './routes';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3333, () => {});
