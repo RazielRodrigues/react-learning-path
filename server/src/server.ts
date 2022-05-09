8f75070ebb4b2a1612b52950b51e3575bf83fa55
razielrodrigues <raziel@upoutsourcing.com>
2022-05-08 23:56:18 -0300

    test

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import { routes } from './routes';
 import cors from 'cors';
 
+const corsOptions = {
+    origin: 'https://react-learning-path.vercel.app/',
+}
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 app.listen(process.env.PORT || 3333, () => {});
