75d6c43d28689672b2f800d92bf7249d598cb84b
razielrodrigues <raziel@upoutsourcing.com>
2022-05-11 00:22:45 -0300

    wip

--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -13,7 +13,7 @@ export function HomeLayout() {
                 className="rounded-full mx-auto m-5 w-48"
                 src="https://avatars.githubusercontent.com/u/40905332?s=400&u=446bae2f3a5d45cfce9bf94690a86d4877b5ea1d&v=4" />
             
-            <h2 className="text-center m-2 text-slate-300">Other Projects</h2>
+            {/* <h2 className="text-center m-2 text-slate-300">Other Projects</h2>
 
             <div className="flex justify-center" >
                 <div>
@@ -33,7 +33,7 @@ export function HomeLayout() {
                     <a className="m-2 rounded-lg text-slate-300 font-medium hover:text-slate-600 transition-colors" href="https://raziel-pure-php.herokuapp.com/">Pure PHP projects</a>
                     <a className="m-2 rounded-lg text-slate-300 font-medium hover:text-slate-600 transition-colors" href="https://raziel-innout-php.herokuapp.com/">Pure PHP MVC Project</a>
                 </div>
-            </div>
+            </div> */}
 
         </div>
     )
