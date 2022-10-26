fff1002ac7dd3436106951412bd3b89887ec5e82
Raziel Rodrigues <razielx3@live.com>
2022-10-26 19:17:03 -0300

    refresh

--- server/src/repositories/feedback-repositories.ts
+++
@@ -1,9 +0,0 @@
-export interface FeedbackCreateData{
-    type: string;
-    comment: string;
-    screenshot?: string;
-}
-
-export interface FeedbackRepository {
-    create: (data: FeedbackCreateData) => Promise<void>;
-}
--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CircleNotch } from "phosphor-react";
-
-export function Loading() {
-    return (
-        <div className="w-6 h-6 items-center justify-center overflow-hidden">
-            <CircleNotch weight="bold" className="w-6 h-6 animate-spin" />
-        </div>
-    );
-}
