005a88471a96a3c806d5a7703caaaaba39cd7fe2
Raziel Rodrigues <razielx3@live.com>
2022-10-26 19:28:19 -0300

    primeiro componente

---
+++ fundamentos/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Primeiro from './components/Primeiro';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Primeiro />
+    </div>
+  );
+}
+
+export default App;
---
+++ fundamentos/src/components/Primeiro.js
@@ -0,0 +1,12 @@
+const Primeiro = () => {
+    return (
+        <div>
+            <h1 className="logo">
+                TESTE Primeiro
+            </h1>
+        </div>
+
+    )
+}
+
+export default Primeiro;
