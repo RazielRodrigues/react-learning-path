6b73116f13de3832390e958ab08d411b11cdd4ac
Raziel Rodrigues <razielx3@live.com>
2022-10-29 13:12:09 -0300

    props

--- fundamentos_2/src/App.js
+++ fundamentos_2/src/App.js
@@ -5,13 +5,24 @@ import Naruto from './assets/naruto.png'
 import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import Conditional from './components/Conditional';
+import Username from './components/Username';
+import { useState } from 'react';
 
 function App() {
+
+  const testeNome = 'Teste nome'
+
+  let [nomeHook, setNomeHoook] = useState('Jao')
+
   return (
     <div className="App">
       <ManageData />
       <ListRender />
       <Conditional />
+      {/* <Username name="IGOR" /> // forma 1 -- passando direto */}
+      {/* <Username name={testeNome} /> // forma 2 -- passando via varaivel */}
+      {/* <Username name={nomeHook} /> // forma 1 -- passando via hook */}
+      <Username name={nomeHook} />
     </div>
   );
 }
