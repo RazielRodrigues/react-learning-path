6651c7137b468656633a07c8a53d3b64e8b61f2c
Raziel Rodrigues <razielx3@live.com>
2022-10-29 14:29:11 -0300

    css

---
+++ fundamentos_css/src/components/CssComponent.js
@@ -0,0 +1,13 @@
+import './CssComponent.css';
+import React from 'react'
+
+function CssComponent() {
+    return (
+        <div>
+
+            <h1 class="a" ></h1>
+        </div>
+    )
+}
+
+export default CssComponent
