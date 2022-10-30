29d58b52800898cf4390910fd97ea40172208682
Raziel Rodrigues <razielx3@live.com>
2022-10-30 13:37:46 -0300

    start screen

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
 
 function App() {
   return (
     <div className="App">
+      <StartScreen />
+      {/* <Game />
+      <GameOver /> */}
     </div>
   );
 }
---
+++ forms/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function StartScreen() {
+    return (
+        <div className='start'>
+            <h1>Secret Word</h1>
+            <p>Clique no botão abaixo para começar a jogar</p>
+            <button>Começar o jogo</button>
+        </div>
+    )
+}
+
+export default StartScreen
