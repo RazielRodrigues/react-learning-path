d3cfa71e483801ae0e3a4b9362e02caa7c63271d
Raziel Rodrigues <razielx3@live.com>
2022-10-30 13:30:40 -0300

    iniciando o projetinho

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
-import MyForm from './components/MyForm';
 
 function App() {
   return (
     <div className="App">
-      <MyForm user={{ name: 'raziel', email: 'razielx3@live.com', bio: 'TA LIGADO', role: 'night' }} />
     </div>
   );
 }
