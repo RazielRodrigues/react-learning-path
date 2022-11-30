520f694aa23d5e833f2b7cd22ddfd8a948361c33
Raziel Rodrigues <razielx3@live.com>
2022-11-30 02:03:52 -0300

    exibindo dados com react pegando de uma api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
+const url = 'http://localhost:3000/products';
+
 function App() {
+
+  // Definindo os produtos
+  const [products, setProducts] = useState([]);
+
+  // Pegando dados utilizando o useEffect
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+    }
+
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
       <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - R$: {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
