e3f5c7c971b51af869111fdfaaace8048b8ba2d0
Raziel Rodrigues <razielx3@live.com>
2022-12-04 13:59:45 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>React router</h1>
 
+      {/* TODOS OS ELEMENTOS QUE QUEREMOS QUE REPITAM EM TODAS
+      AS PAGINAS DEVEM FICAR DENTRO DEO BROWSER ROUTER */}
       <BrowserRouter>
+        <Navbar></Navbar>
+
         <Routes>
           <Route path="/about" element={<About />}></Route>
           <Route path="/" element={<Home />}></Route>
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Navbar.css';
+
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to="/"> Home </Link>
+            <Link to="/about"> Sobre </Link>
+        </nav>
+    )
+}
+
+export default Navbar
