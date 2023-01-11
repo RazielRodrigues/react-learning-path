b3badea80aca9df343c93be5a4d2a53d4d2f0846
Raziel Rodrigues <razielx3@live.com>
2023-01-10 22:07:38 -0300

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Home } from './pages/Home/Home';
+import { About } from './pages/About/About';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { Login } from './pages/Login/Login';
+import { Register } from './pages/Register/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
