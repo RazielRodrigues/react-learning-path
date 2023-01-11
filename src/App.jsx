5447f082821c0bb7eeb5c2727b15782efa360389
Raziel Rodrigues <razielx3@live.com>
2023-01-11 00:43:08 -0300

    adding auth to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { Home } from './pages/Home/Home';
@@ -7,22 +7,48 @@ import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
+import { AuthProvider } from './context/AuthContext';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useAuthentication } from './hooks/useAuthentication';
+import { CreatePost } from './pages/CreatePost/CreatePost';
+import { Dashboard } from './pages/Dashboard/Dashboard';
 
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const { auth } = useAuthentication()
+
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+  }, [auth])
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider value={{ user }}>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+              <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
+              <Route path="/posts/create" element={user ? <CreatePost /> : <Navigate to="/" />} />
+              <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/" />} />
+              <Route path="/logout" element={user ? <Dashboard /> : <Navigate to="/" />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import './Navbar.css'
 import { NavLink } from "react-router-dom";
 
+import { useAuthentication } from '../hooks/useAuthentication';
+import { useAuthValue } from '../context/AuthContext';
+
 export const Navbar = () => {
+
+    const { user } = useAuthValue();
+    const { logout } = useAuthentication();
+
     return (
         <nav className='navbar'>
 
@@ -10,23 +17,49 @@ export const Navbar = () => {
                 Mini <span>Blog</span>
             </NavLink>
 
-            <ul className='links_list'>
-                <li>
-                    <NavLink to="/about">
-                        Sobre
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/login">
-                        Login
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register">
-                        Cadastrar
-                    </NavLink>
-                </li>
-            </ul>
+            {!user && (
+                <ul className='links_list'>
+                    <li>
+                        <NavLink to="/about">
+                            Sobre
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login">
+                            Login
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/register">
+                            Cadastrar
+                        </NavLink>
+                    </li>
+                </ul>
+            )}
+
+            {user && (
+                <ul className='links_list'>
+                    <li>
+                        <NavLink to="/posts/create">
+                            Criar post
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/dashboard">
+                            Dashboard
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/logout">
+                            Dashboard
+                        </NavLink>
+                    </li>
+                    <li>
+                        <button onClick={logout}>Logout</button>
+                    </li>
+                </ul>
+            )}
+
         </nav>
     )
 }
