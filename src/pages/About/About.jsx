43e4a9b1ade1919b31f5634d6882f12a82568300
Raziel Rodrigues <razielx3@live.com>
2023-01-11 00:43:37 -0300

    page edit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import './About.css'
+import { Link } from 'react-router-dom'
 
 export const About = () => {
     return (
-        <div>About</div>
+        <div className='about'>
+            <h2>
+                Projeto feito com REACT
+            </h2>
+            <p>
+                CRIE SEUS POSTS!
+            </p>
+            <Link to="/posts/create" className="btn">
+                Criar post
+            </Link>
+        </div>
     )
 }
